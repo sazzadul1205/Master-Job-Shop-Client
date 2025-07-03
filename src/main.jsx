973d9e5f53f7e313069b54051069a8f3b91a6502
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2025-07-03 18:33:12 +0600

    chore: update dependencies and configuration for improved performance and features
    - Updated dependencies in package.json including Tailwind CSS, Firebase, React Router, and ESLint.
    - Modified postcss.config.js to use the new Tailwind CSS PostCSS plugin.
    - Changed base URL in useAxiosPublic hook for local development.
    - Updated index.css to import Tailwind CSS and DaisyUI correctly.
    - Refactored main.jsx to use React Router v7 with new routing structure.
    - Updated vite.config.js to use the SWC version of the React plugin.
    - Added new vite.config.ts file for TypeScript support with Vite and Tailwind CSS.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/Routes";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./Provider/AuthProvider";
+import MainLayout from "./Layouts/MainLayout";
+import Home from "./Pages/Home/Home";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +15,21 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Routes>
+            {/* Home Page Layout */}
+            <Route element={<MainLayout />}>
+              {/* Home Pages Link */}
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        {/* <RouterProvider router={router} /> */}
+        {/* <Routes></Routes> */}
+        {/* Home Page Layout */}
+        {/* <Route element={<HomePageLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route> */}
       </QueryClientProvider>
     </AuthProvider>
   </StrictMode>

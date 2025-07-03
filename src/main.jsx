1b24ffc81978406733ae28af2363d085fb4d7615
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2025-07-03 16:28:57 +0600

    chore: update dependencies and configuration for Tailwind CSS and Vite
    - Added @tailwindcss/vite and @vitejs/plugin-react-swc to dependencies.
    - Upgraded Firebase to version 11.10.0 and react-router-dom to version 6.30.1.
    - Updated daisyui to version 5.0.43 and tailwindcss to version 3.4.17.
    - Modified postcss.config.js to use @tailwindcss/postcss.
    - Refactored index.css to include compatibility styles for Tailwind CSS v4.
    - Changed routing implementation in main.jsx to use BrowserRouter and Routes.
    - Created a new vite.config.ts file for Vite configuration.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import "./index.css";
 import { StrictMode } from "react";
-import { router } from "./Routes/Routes";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import AuthProvider from "./Provider/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import MainLayout from "./Layouts/MainLayout";
+import Home from "./Pages/Home/Home";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,15 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Routes>
+            {/* Home Page Layout */}
+            <Route element={<MainLayout />}>
+              {/* Home Pages Link */}
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </AuthProvider>
   </StrictMode>

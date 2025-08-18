062523cb85be82e3196d5e4a8f885a9618a6e531
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2025-08-18 21:47:33 +0600

    - Updated Employer Dashboard Page - Added Fetch Query for Fetching Data

--- src/Pages/(Employer_Pages)/EmployerDashboard/EmployerDashboard.jsx
+++ src/Pages/(Employer_Pages)/EmployerDashboard/EmployerDashboard.jsx
@@ -1,11 +1,160 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import EmployerDashboardKPI from "./EmployerDashboardKPI/EmployerDashboardKPI";
+import Error from "../../../Shared/Error/Error";
+import Loading from "../../../Shared/Loading/Loading";
 
 
 const EmployerDashboard = () => {
-    return (
-        <div>
+  const { user, loading } = useAuth();
+  const axiosPublic = useAxiosPublic();
 
-        </div>
-    );
+  // Daily Gig Status Data
+  const {
+    data: DailyGigStatusData = [],
+    isLoading: DailyGigStatusIsLoading,
+    error: DailyGigStatusError,
+    refetch: DailyGigStatusRefetch,
+  } = useQuery({
+    queryKey: ["DailyGigStatusData"],
+    queryFn: async () => {
+      try {
+        const res = await axiosPublic.get(`/Gigs/DailyGigPosted?postedBy=${user.email}`);
+        return res.data || [];
+      } catch (error) {
+        if (error.response?.status === 404) return [];
+        throw error;
+      }
+    },
+    enabled: !!user?.email,
+  });
+
+  // Daily Event Status Data
+  const {
+    data: DailyEventStatusData = [],
+    isLoading: DailyEventStatusIsLoading,
+    error: DailyEventStatusError,
+    refetch: DailyEventStatusRefetch,
+  } = useQuery({
+    queryKey: ["DailyEventStatusData"],
+    queryFn: async () => {
+      try {
+        const res = await axiosPublic.get(`/Events/DailyEventsPosted?postedBy=${user.email}`);
+        return res.data || [];
+      } catch (error) {
+        if (error.response?.status === 404) return [];
+        throw error;
+      }
+    },
+    enabled: !!user?.email,
+  });
+
+  // GET Ids
+  // Gig Ids Data
+  const {
+    data: GigIdsData = [],
+    isLoading: GigIdsIsLoading,
+    error: GigIdsError,
+    refetch: GigIdsRefetch,
+  } = useQuery({
+    queryKey: ["GigIdsData"],
+    queryFn: async () => {
+      try {
+        const res = await axiosPublic.get(`/Gigs/Ids?postedBy=${user.email}`);
+        return res.data || [];
+      } catch (error) {
+        if (error.response?.status === 404) return [];
+        throw error;
+      }
+    },
+    enabled: !!user?.email,
+  });
+
+  // Event Ids Data
+  const {
+    data: EventIdsData = [],
+    isLoading: EventIdsIsLoading,
+    error: EventIdsError,
+    refetch: EventIdsRefetch,
+  } = useQuery({
+    queryKey: ["EventIdsData"],
+    queryFn: async () => {
+      try {
+        const res = await axiosPublic.get(`/Events/Ids?postedBy=${user.email}`);
+        return res.data || [];
+      } catch (error) {
+        if (error.response?.status === 404) return [];
+        throw error;
+      }
+    },
+    enabled: !!user?.email,
+  });
+
+  // Daily Event Status Data
+  const {
+    data: DailyEventApplicationsStatusData = [],
+    isLoading: DailyEventApplicationsStatusIsLoading,
+    error: DailyEventApplicationsStatusError,
+    refetch: DailyEventApplicationsStatusRefetch,
+  } = useQuery({
+    queryKey: ["DailyEventApplicationsStatusData"],
+    queryFn: async () => {
+      try {
+        const res = await axiosPublic.get(`/eventApplications/DailyEventApplicationsPosted?eventIds=${EventIdsData}`);
+        return res.data || [];
+      } catch (error) {
+        if (error.response?.status === 404) return [];
+        throw error;
+      }
+    },
+    enabled: !!user?.email,
+  });
+
+  console.log("Daily Event Applications Status Data :", DailyEventApplicationsStatusData);
+  // console.log("Event Ids Data :", EventIdsData);
+
+  // Refetch both datasets
+  const refetch = async () => {
+    await GigIdsRefetch();
+    await EventIdsRefetch();
+    await DailyGigStatusRefetch();
+    await DailyEventStatusRefetch();
+    await DailyEventApplicationsStatusRefetch();
+  };
+
+  // If Loading Show UI
+  if (
+    loading ||
+    GigIdsIsLoading ||
+    EventIdsIsLoading ||
+    DailyGigStatusIsLoading ||
+    DailyEventStatusIsLoading ||
+    DailyEventApplicationsStatusIsLoading)
+    return <Loading />;
+
+  // If Error Show UI
+  if (
+    GigIdsError ||
+    EventIdsError ||
+    DailyGigStatusError ||
+    DailyEventStatusError ||
+    DailyEventApplicationsStatusError)
+    return <Error />;
+
+  return (
+    <div>
+      {/* Employer Dashboard KPI */}
+      <EmployerDashboardKPI
+        refetch={refetch}
+        DailyGigStatusData={DailyGigStatusData}
+        DailyEventStatusData={DailyEventStatusData}
+
+      />
+
+
+    </div>
+  );
 };
 
 export default EmployerDashboard;
---
+++ src/Pages/(Employer_Pages)/EmployerDashboard/EmployerDashboardKPI/EmployerDashboardKPI.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployerDashboardKPI = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EmployerDashboardKPI;

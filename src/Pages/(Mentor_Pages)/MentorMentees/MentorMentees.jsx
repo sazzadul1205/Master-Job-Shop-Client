8d0c62f0b1f272c5d8a43b656f90386d8a90b7ec
Sazzadul Islam <psazzadul@gmail.com>
2025-09-23 17:36:04 +0600

    - Updated MentorMentees Page -- Created Filter Component

--- src/Pages/(Mentor_Pages)/MentorMentees/MentorMentees.jsx
+++ src/Pages/(Mentor_Pages)/MentorMentees/MentorMentees.jsx
@@ -5,6 +5,8 @@ import Loading from "../../../Shared/Loading/Loading";
 import Error from "../../../Shared/Error/Error";
 
 import MentorMenteesCard from "./MentorMenteesCard/MentorMenteesCard";
+import { FaSearch } from "react-icons/fa";
+import { FaRegMessage } from "react-icons/fa6";
 
 const MentorMentees = () => {
   const { user, loading } = useAuth();
@@ -125,9 +127,6 @@ const MentorMentees = () => {
     ? Object.values(MyMentorshipApplicationsData).flat()
     : [];
 
-  console.log("Course :", courseApplications);
-  console.log("Mentorship :", mentorshipApplications);
-
   return (
     <div>
       {/* Header */}
@@ -141,6 +140,72 @@ const MentorMentees = () => {
         courseApplications={courseApplications}
         mentorshipApplications={mentorshipApplications}
       />
+
+      {/* Filter */}
+      <div className="mx-7 my-9">
+        <div className="w-full flex flex-wrap items-center gap-4 rounded-xl bg-white border border-gray-300 p-5 shadow-sm">
+          {/* Search By Mentees Name */}
+          <div className="relative flex-1 min-w-[220px]">
+            <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+            <input
+              type="search"
+              required
+              placeholder="Search by Mentees Name..."
+              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg bg-white 
+       focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
+       placeholder-gray-400 text-sm text-black transition duration-200"
+            />
+          </div>
+
+          {/* Search By Course / Mentorship Name */}
+          <div className="relative flex-1 min-w-[220px]">
+            <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+            <input
+              type="search"
+              required
+              placeholder="Search by Course / Mentorship Name..."
+              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg bg-white 
+       focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
+       placeholder-gray-400 text-sm text-black transition duration-200"
+            />
+          </div>
+
+          {/* Dropdown Filter */}
+          <select
+            name="filter"
+            id="filter"
+            className="min-w-[150px] rounded-lg border border-gray-300 bg-white px-3 py-2 
+     text-sm text-gray-700 shadow-sm focus:border-blue-500 focus:ring-2 
+     focus:ring-blue-500 focus:outline-none transition duration-200"
+          >
+            <option value="">Select Option</option>
+            <option value="courses">Courses</option>
+            <option value="mentorship">Mentorship</option>
+          </select>
+
+          {/* Action Buttons */}
+          <div className="flex items-center gap-3">
+            <button
+              type="button"
+              className="px-4 py-2 bg-gray-100 border border-gray-300 text-sm text-gray-700 rounded-lg 
+       hover:bg-gray-200 transition-colors cursor-pointer"
+            >
+              Clear Filter
+            </button>
+
+            <button
+              type="button"
+              className="flex items-center gap-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 
+       border border-blue-600 text-sm text-white font-medium rounded-lg 
+       transition-colors cursor-pointer"
+            >
+              <FaRegMessage className="text-base" />
+              <span>Bulk Message</span>
+              <span className="opacity-80">(None)</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
